9c18f297bde3f95fe427183057053f9e6024cceb
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-25 13:28:11 +0900

    Add:バリデーションを作成

--- app/models/near_station.rb
+++ app/models/near_station.rb
@@ -1,3 +1,6 @@
 class NearStation < ApplicationRecord
   belongs_to :property
+  validates :name, presence: true,
+  validates :route, presence: true,
+  validates :required_time, presence: true,
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ApplicationRecord
-  has_many :near_stations
+  has_many :near_stations, dependent: :destory
   accepts_nested_attributes_for :near_stations
+  validates :name, presence: true,
+  validates :rent, presence: true,
+  validates :address, presence: true,
+  validates :age, presence: true
 end
