5794aec1c653a0f173f764b6870a1c9c3107082a
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-25 18:15:43 +0900

    Add:子のフォームで路線名と駅名が未入力の場合、その子レコードはsaveとupdateを行わない制限を追加(reject_if)

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
   has_many :near_stations, dependent: :destroy
-  accepts_nested_attributes_for :near_stations, allow_destroy: true
+  accepts_nested_attributes_for :near_stations, reject_if: lambda {|attributes| attributes[:name].blank? || attributes[:route].blank?} , allow_destroy: true
 end
