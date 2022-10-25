083ac3bb3cbad4ba6654a24a735a49645b579ad5
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-25 12:08:57 +0900

    Add:モデルを作成

---
+++ db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_10_25_025603) do
+
+  create_table "near_stations", force: :cascade do |t|
+    t.string "name"
+    t.string "route"
+    t.integer "required_time"
+    t.integer "property_id"
+    t.index ["property_id"], name: "index_near_stations_on_property_id"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.integer "rent"
+    t.string "address"
+    t.integer "age"
+    t.text "note"
+  end
+
+  add_foreign_key "near_stations", "properties"
+end
